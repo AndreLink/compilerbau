0a511e2a4a2005ba608a6fbba6accd1fc32a7c87
Andre <rubikwuerfel1@web.de>
2019-06-01 18:38:04 +0200

    0106: fix1 conditionals

--- src/programTree.c
+++ src/programTree.c
@@ -1216,7 +1216,7 @@ void add_helper_assign_expression_to_list(nt_stmt_list *new_statement_list, nt_e
     add_to_ll(new_statement_list->statements, ntf_stmt_3(ret));
 }
 
-nt_stmt *create_goto(int goto_label, nt_expression *condition)
+nt_stmt_goto *create_goto(int goto_label, nt_expression *condition)
 {
     nt_stmt_goto *ret = malloc(sizeof(nt_stmt_goto));
     ret->self = STMT_GOTO;
@@ -1227,9 +1227,9 @@ nt_stmt *create_goto(int goto_label, nt_expression *condition)
     return ret;
 }
 
-nt_stmt *create_label(int label)
+nt_stmt_label *create_label(int label)
 {
-    nt_stmt_goto *ret = malloc(sizeof(nt_stmt_label));
+    nt_stmt_label *ret = malloc(sizeof(nt_stmt_label));
     ret->self = STMT_LABEL;
     char *label = malloc(10 * sizeof(char));
     snprintf(label, 10, "__label%d", label);
